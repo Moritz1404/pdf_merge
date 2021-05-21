6c3c127a2fe077d504d802e4b1e109e944b021be
Moritz Steinbusch <moritzsteinbusch@gmail.com>
2021-05-21 15:33:38 +0200

    added error popUp

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from tkinter.constants import VERTICAL
+from traceback import clear_frames
 from merge import check_file, merge_pdf, open_file
 import PySimpleGUI as sg
 
 if __name__=='__main__':
     sg.theme('DarkAmber') #Color theme
     #Alles innerhalb des Fensters
-    layout = [  [sg.Text('First PDF path:'), sg.InputText()],
-                [sg.Text('Second PDF path:'), sg.InputText()],
+    layout = [  [sg.Text('First PDF path:'), sg.InputText(key='Input1')],
+                [sg.Text('Second PDF path:'), sg.InputText(key='Input2')],
                 [sg.Button('Merge'), sg.Button('Cancel')]
     ]
     #Create the window
@@ -17,9 +18,13 @@ if __name__=='__main__':
         if event == sg.WIN_CLOSED or event == 'Cancel':
             break
 
-        if check_file([values[0], values[1]]) is True:
-            merge_pdf([values[0], values[1]], "out.pdf")
+        if check_file([values['Input1'], values['Input2']]) is True:
+            merge_pdf([values['Input1'], values['Input2']], "out.pdf")
             open_file("out.pdf")
-            break    
+            break
+        else:
+            sg.popup('Wrong Path', 'Enter a valid path')    
+            window['Input1'].update('')
+            window['Input2'].update('')
 
     window.close()
